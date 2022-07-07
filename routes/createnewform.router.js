8123ffc807c8a55b8388e2ba01ee54ef621741b1
Emiliya <emilya.korkunova@gmail.com>
2022-07-07 18:03:38 +0300

    fix

--- routes/createnewform.router.js
+++ routes/createnewform.router.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
+const { Trip } = require("../db/models")
 
 router.get('/', (req, res) => { // id 
   res.render('createnewform');
 });
 
-router.post('/', (req, res) => {
-  console.log(11111);
+router.post('/', async (req, res) => {
+  const { nameform, length, start, finish, location} = req.body;
   console.log(req.body);
+  await Trip.create({ 
+    name:nameform, 
+    user_id: req.session.user_id,
+    length,
+    start,
+    finish,
+    location});
+    
+    res.json({nameform, length, start, finish, location})
     
 })
 
