b20195ab4bd29c03b295d5d4e3932386b4a94088
Khakhonina Maria <khakhoninama@gmail.com>
2022-07-07 11:32:27 +0300

    signup-signin-logout

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const indexRouter = require('./routes/index.router');
 const mainPageRouter = require('./routes/mainPage.router');
-const signup = require('./routes/signup.router')
-const signin = require('./routes/signin.router')
+const signup = require('./routes/signup.router');
+const signin = require('./routes/signin.router');
+const logout = require('./routes/logout.router');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,7 +23,6 @@ const sessionConfig = {
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 12,
     httpOnly: true,
   },
 };
@@ -38,11 +38,15 @@ hbs.registerPartials(path.join(process.env.PWD, 'views', 'partials'));
 app.use(cookieParser());
 
 // место для middleware
-
 // место для ручек
 app.get('/', indexRouter);
 app.get('/cycling-trips', mainPageRouter);
-app.use('/cycling-trips/signup', signup);
-app.use('/cycling-trips/signin', signin);
+app.use('/signup', signup);
+app.use('/signin', signin);
+app.use('/logout', logout);
 
+app.use((req, res, next) => {
+  res.locals.user = req.session?.user;
+  next();
+});
 app.listen(PORT, () => console.log(`Connection on PORT: ${PORT}`));
