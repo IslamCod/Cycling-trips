19879aab2f2eb89641028beb617a165f92f4d44f
Emiliya <emilya.korkunova@gmail.com>
2022-07-07 18:45:09 +0300

    add routes in base

--- routes/createnewform.router.js
+++ routes/createnewform.router.js
@@ -6,17 +6,23 @@ router.get('/', (req, res) => { // id
 });
 
 router.post('/', async (req, res) => {
-  const { nameform, length, start, finish, location} = req.body;
-  console.log(req.body);
-  await Trip.create({ 
-    name:nameform, 
-    user_id: req.session.user_id,
-    length,
-    start,
-    finish,
-    location});
-    
-    res.json({nameform, length, start, finish, location})
+  const { title, length, start, finish, location} = req.body;
+
+  try {
+    console.log(req.body);
+    await Trip.create({ 
+      title, 
+      user_id: req.session.user_id,
+      length,
+      start: JSON.stringify(start),
+      finish: JSON.stringify(finish),
+      location});
+      
+      res.json({title, length, start, finish, location})
+  } catch (er) {
+    console.log(er);
+  }
+  
     
 })
 
