3977fd0e263890eaf7a94b6c75a26696e7f1e240
Emiliya <emilya.korkunova@gmail.com>
2022-07-08 11:23:21 +0300

    add map with routes:fix

--- routes/mainPage.router.js
+++ routes/mainPage.router.js
@@ -11,7 +11,7 @@ router.get('/cycling-trips', async (req, res) => {
   res.render('mainPage', { allTrips });
 });
 
-router.get('/cycling-trips/:id', async (req, res) => {
+router.get('/cycling-trips/:id', async (req, res) => { //поиск маршрута для отрисовки его карты и подробной информации - Миля
   const trip = await Trip.findOne({where: {id: req.params.id}});
   res.json(trip);
 });
